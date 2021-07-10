7565a4a67a0f85aa79bb3cf1a95d3344400f2a38
Joe Lorenzetti <jlorenze@stanford.edu>
2021-07-09 17:23:52 -0700

    small updates to plotting

--- data/skywalker.py
+++ data/skywalker.py
@@ -5,11 +5,17 @@ import numpy as np
 from scipy.interpolate import interp1d
 import pdb
 
-from utils import get_data_dir, RosbagData, aadot_to_om, get_models_dir
+from utils import get_data_dir, RosbagData, get_models_dir, get_utils_dir
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
+    if len(sys.argv) == 2:
+        t0 = tf = None
+    elif len(sys.argv) == 4:
+        t0 = float(sys.argv[2])
+        tf = float(sys.argv[3])
+    else:
         print('Correct usage: python skywalker.py {filename}.bag')
+        print('or: python skywalker.py {filename}.bag t0 t1')
         sys.exit()
 
     data_dir = get_data_dir()
@@ -20,9 +26,10 @@ if __name__ == '__main__':
     data = RosbagData(fpath)
 
     # Find beginning and end of flight
-    idx = np.where(np.abs(np.array(data.plane.act.u[3])) > 0.0)[0]
-    t0 = data.plane.act.t[idx[0]]
-    tf = data.plane.act.t[idx[-1]]
+    if t0 is None:
+        idx = np.where(np.abs(np.array(data.plane.act.u[3])) > 0.0)[0]
+        t0 = data.plane.act.t[idx[0]]
+        tf = data.plane.act.t[idx[-1]]
 
     # Compute thrust estimate
     sys.path.append(join(get_models_dir(), "skywalker"))
@@ -31,7 +38,9 @@ if __name__ == '__main__':
     c_T0, c_TVom = p[0], p[1]
     T_est = []
     f = interp1d(data.plane.vel.t, data.plane.vel.x, bounds_error=False, fill_value='extrapolate')
-    for t, u_T in zip(data.plane.act.t, data.plane.nrmlzd_act.u[3]):
+    for t, u_T in zip(data.plane.nrmlzd_act.t, data.plane.nrmlzd_act.u[3]):
+        # if t > 170:
+        #     pdb.set_trace()
         if u_T < .0001:
             T_est.append(0.0)
         else:
@@ -42,7 +51,17 @@ if __name__ == '__main__':
     u_eq = np.loadtxt(fpath, delimiter=",")
     T_rompc = np.array(data.rompc.u.u[0]) + u_eq[0]
 
+    # Convert axis/angle to euler angles relative to target frame
+    sys.path.append(get_utils_dir())
+    from rotation import aadot_to_om, axis_to_quat, quat_to_euler, quat_to_axis, euler_to_quat
+    e_att = np.vstack((data.rompc.e_att.x, data.rompc.e_att.y, data.rompc.e_att.z))
+    euler_rompc = np.empty((0,3))
+    for i, t in enumerate(data.rompc.e_att.t):
+        e_att_euler = quat_to_euler(axis_to_quat(e_att[:,i]))
+        euler_rompc = np.vstack((euler_rompc, e_att_euler))
+
     # Convert axis/angle rate commands to body rates
+    # NOTE THAT THIS ASSUMES THE TARGET ANGULAR VELOCITY IS ZERO
     e_att = np.vstack((data.rompc.e_att.x, data.rompc.e_att.y, data.rompc.e_att.z))
     f = interp1d(data.rompc.e_att.t, e_att, bounds_error=False, fill_value='extrapolate')
     om_rompc = np.empty((0,3))
@@ -93,7 +112,7 @@ if __name__ == '__main__':
     axs2[2].plot(data.plane.vel.t, data.plane.vel.V, 'r', label='plane.V')
     axs2[2].set_ylabel('Velocity [m/s]')
 
-    axs2[3].plot(data.plane.act.t, T_est, 'r')
+    axs2[3].plot(data.plane.nrmlzd_act.t, T_est, 'r')
     axs2[3].set_ylabel('Thrust Estimate [N]')
 
 
@@ -103,7 +122,7 @@ if __name__ == '__main__':
 
 
     # Plot plane related data streams
-    fig, axs3 = plt.subplots(3, 1, sharex='col')
+    fig, axs3 = plt.subplots(4, 1, sharex='col')
     axs3[0].plot(data.rompc.e_pos.t, data.rompc.e_pos.x, 'r', label='rompc.e_x')
     axs3[0].plot(data.rompc.e_pos.t, data.rompc.e_pos.y, 'g', label='rompc.e_y')
     axs3[0].plot(data.rompc.e_pos.t, data.rompc.e_pos.z, 'b', label='rompc.e_z')
@@ -127,6 +146,11 @@ if __name__ == '__main__':
     axs3[2].plot(data.rompc.zhat.t, np.rad2deg(data.rompc.zhat.z[7]), 'g*', label='rompc.e_att_y_est')
     axs3[2].plot(data.rompc.zhat.t, np.rad2deg(data.rompc.zhat.z[8]), 'b*', label='rompc.e_att_z_est')
     axs3[2].set_ylabel('Attitude error [deg]')
+
+    axs3[3].plot(data.rompc.e_att.t, np.rad2deg(euler_rompc[:,0]), 'r', label='rompc.e_att_phi')
+    axs3[3].plot(data.rompc.e_att.t, np.rad2deg(euler_rompc[:,1]), 'g', label='rompc.e_att_th')
+    axs3[3].plot(data.rompc.e_att.t, np.rad2deg(euler_rompc[:,2]), 'b', label='rompc.e_att_psi')
+    axs3[3].set_ylabel('Euler angle error [deg]')
 
     for ax in axs3:
         ax.legend()
--- src/utils/rotation.py
+++ src/utils/rotation.py
@@ -146,6 +146,63 @@ def invert_quat(q):
     """
     return np.array([q[0], -q[1], -q[2], -q[3]])
 
+def tangential_transf(aa):
+    """
+    @brief Computes operator T such that om = T(aa) * aa_dot which
+    transforms the rate of change of axis/angle representation of
+    rotation from A to B into the angular velocity of B w.r.t A written
+    in B coordinates.
+
+    @param[in] aa  axis angle representation aa = (th1, th2, th3)
+    """
+    th2 = np.linalg.norm(aa)**2
+
+    if th2 < 5e-6:
+        th4 = th2 * th2
+        c1 = 1.0  - th2/6   + th4/120
+        c2 = 1/2. - th2/24  + th4/720
+        c3 = 1/6. - th2/120 + th4/5040
+    else:
+        th = np.sqrt(th2)
+        c1 = np.sin(th)/th
+        c2 = (1 - np.cos(th))/th2
+        c3 = (th - np.sin(th))/(th * th2)
+
+    skew_th = np.array([[0.0, -aa[2], aa[1]],
+                        [aa[2], 0.0, -aa[0]],
+                        [-aa[1], aa[0], 0.0]])
+
+
+    T = c1*np.eye(3) - c2*skew_th + c3*np.outer(aa, aa)
+    return T
+
+def om_to_aadot(aa, om):
+    """
+    @brief Converts the angular velocity vector om for a frame B rotating w.r.t A
+    into the rate of change of the axis/angle parameters that represent the
+    rotation from A to B.
+
+    @param[in] aa     axis/angle repr. of rotation from A to B frames [rad]
+    @param[in] om     angular velocity of B w.r.t A [rad/s]
+    @param[in] aadot  time derivative of aa [rad/s]
+    """
+    T = tangential_transf(aa)
+    aadot = np.matmul(np.linalg.inv(T), om)
+    return aadot
+
+def aadot_to_om(aa, aadot):
+    """
+    @brief Given current axis/angle parameters for rotation from A to B and their
+    time derivatives, computes the angular velocity vector of B w.r.t A.
+
+    @param[in] aa     axis/angle repr. of rotation from A to B frames [rad]
+    @param[in] aadot  time derivative of aa [rad/s]
+    @param[in] om     angular velocity of B w.r.t A [rad/s]
+    """
+    T = tangential_transf(aa)
+    om = np.matmul(T, aadot);
+    return om
+
 
 if __name__ == '__main__':
     # Testing quat to euler
